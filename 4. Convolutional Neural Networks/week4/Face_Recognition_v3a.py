013e5a4a20c234d1ee4fbbbb18a9479be32c7608
Siarhei Antaniuk <katilinas@gmail.com>
2020-12-24 21:54:04 +0100

    Face Recognition done

--- 4. Convolutional Neural Networks/week4/Face_Recognition_v3a.py
+++ 4. Convolutional Neural Networks/week4/Face_Recognition_v3a.py
@@ -40,7 +40,7 @@
 # #### Load packages
 # Let's load the required packages. 
 
-# In[ ]:
+# In[25]:
 
 from keras.models import Sequential
 from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
@@ -93,12 +93,12 @@ np.set_printoptions(threshold=np.nan)
 # 
 # Run the cell below to create the model for face images.
 
-# In[ ]:
+# In[26]:
 
 FRmodel = faceRecoModel(input_shape=(3, 96, 96))
 
 
-# In[ ]:
+# In[27]:
 
 print("Total Params:", FRmodel.count_params())
 
@@ -184,7 +184,7 @@ print("Total Params:", FRmodel.count_params())
 # * In step 4, when summing over training examples, the result will be a single scalar value.
 # * For `tf.reduce_sum` to sum across all axes, keep the default value `axis=None`.
 
-# In[ ]:
+# In[28]:
 
 # GRADED FUNCTION: triplet_loss
 
@@ -207,19 +207,19 @@ def triplet_loss(y_true, y_pred, alpha = 0.2):
     
     ### START CODE HERE ### (≈ 4 lines)
     # Step 1: Compute the (encoding) distance between the anchor and the positive
-    pos_dist = None
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,positive)),axis=-1)
     # Step 2: Compute the (encoding) distance between the anchor and the negative
-    neg_dist = None
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,negative)),axis=-1)
     # Step 3: subtract the two previous distances and add alpha.
-    basic_loss = None
+    basic_loss = pos_dist- neg_dist + alpha
     # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
-    loss = None
+    loss = tf.reduce_sum(tf.maximum(basic_loss, 0.0))
     ### END CODE HERE ###
     
     return loss
 
 
-# In[ ]:
+# In[29]:
 
 with tf.Session() as test:
     tf.set_random_seed(1)
@@ -250,7 +250,7 @@ with tf.Session() as test:
 # 
 # FaceNet is trained by minimizing the triplet loss. But since training requires a lot of data and a lot of computation, we won't train it from scratch here. Instead, we load a previously trained model. Load a model using the following cell; this might take a couple of minutes to run. 
 
-# In[ ]:
+# In[30]:
 
 FRmodel.compile(optimizer = 'adam', loss = triplet_loss, metrics = ['accuracy'])
 load_weights_from_FaceNet(FRmodel)
@@ -330,18 +330,18 @@ def verify(image_path, identity, database, model):
     ### START CODE HERE ###
     
     # Step 1: Compute the encoding for the image. Use img_to_encoding() see example above. (≈ 1 line)
-    encoding = None
+    encoding = img_to_encoding(image_path, model)
     
     # Step 2: Compute distance with identity's image (≈ 1 line)
-    dist = None
+    dist = np.linalg.norm(encoding - database[identity])
     
     # Step 3: Open the door if dist < 0.7, else don't open (≈ 3 lines)
-    if None:
-        print("It's " + str(identity) + ", welcome in!")
-        door_open = None
+    if dist < 0.7:
+        print("It's " + str(identity) + ", welcome home!")
+        door_open = True
     else:
         print("It's not " + str(identity) + ", please go away")
-        door_open = None
+        door_open = False
         
     ### END CODE HERE ###
         
@@ -430,23 +430,23 @@ def who_is_it(image_path, database, model):
     ### START CODE HERE ### 
     
     ## Step 1: Compute the target "encoding" for the image. Use img_to_encoding() see example above. ## (≈ 1 line)
-    encoding = None
+    encoding = img_to_encoding(image_path, model)
     
     ## Step 2: Find the closest encoding ##
     
     # Initialize "min_dist" to a large value, say 100 (≈1 line)
-    min_dist = None
+    min_dist = 100
     
     # Loop over the database dictionary's names and encodings.
-    for (name, db_enc) in None:
+    for (name, db_enc) in database.items():
         
-        # Compute L2 distance between the target "encoding" and the current db_enc from the database. (≈ 1 line)
-        dist = None
+        # Compute L2 distance between the target "encoding" and the current "emb" from the database. (≈ 1 line)
+        dist = np.linalg.norm(encoding-db_enc)
 
         # If this distance is less than the min_dist, then set min_dist to dist, and identity to name. (≈ 3 lines)
-        if None:
-            min_dist = None
-            identity = None
+        if dist < min_dist:
+            min_dist = dist
+            identity = name
 
     ### END CODE HERE ###
     
